f7f8e6c7d2b5427f43b92a1416bed5034298f404
Thirafi Dide <jurnaldide@gmail.com>
2023-09-11 18:34:22 +0700

    Basic idea

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,112 +2,116 @@ import Image from 'next/image';
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-				<p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-					Get started by editing&nbsp;
-					<code className="font-mono font-bold">src/app/page.tsx</code>
-				</p>
-				<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-					<a
-						className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-						href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						By{' '}
-						<Image
-							src="/vercel.svg"
-							alt="Vercel Logo"
-							className="dark:invert"
-							width={100}
-							height={24}
-							priority
+		<div className="min-h-screen bg-[#FCFCFC] p-5">
+			<main className="flex flex-col gap-5 container mx-auto">
+				<div className="flex flex-col md:flex-row gap-3 justify-stretch">
+					<CandidateMainSection>
+						<CandidatePhoto
+							src="/foto-kandidat/Prabowo_Subianto.jpg"
+							alt="Potret Resmi Prabowo Subianto sebagai Mentri Pertahanan Republik Indonesia Periode 2019-Sekarang"
+							width={786}
+							height={1086}
 						/>
-					</a>
+						<PresidentialCandidateName>
+							Prabowo Subianto
+						</PresidentialCandidateName>
+						Gerindra (12,57% | 78), Golkar (12,31% | 85), PAN (6,84% | 44), PBB
+						(0,79% | 0), Gelora, PSI (1,89% | 0), Garuda (0,50% | 0)
+						<br />
+						36% | 207 Kursi
+						<div className="flex flex-wrap gap-2">
+							<PoliticalParty name="Gerindra" />
+							<PoliticalParty name="Golkar" />
+							<PoliticalParty name="PAN" />
+							<PoliticalParty name="PBB" />
+							<PoliticalParty name="Gelora" />
+							<PoliticalParty name="PSI" />
+							<PoliticalParty name="Garuda" />
+						</div>
+					</CandidateMainSection>
+					<CandidateMainSection>
+						<CandidatePhoto
+							src="/foto-kandidat/Anies_Baswedan.jpg"
+							alt="Potret Resmi Anies Baswedan sebagai Gubernur DKI Jakarta Periode 2017-2022"
+							width={1920}
+							height={2398}
+						/>
+						<PresidentialCandidateName>
+							Anies Baswedan
+						</PresidentialCandidateName>
+						Muhaimin Iskandar Nasdem(9,05% | 59), PKB (9,69% | 58), PKS (8,21% |
+						50), Ummat
+						<br />
+						26.95% | 167 Kursi
+						<div className="flex flex-wrap gap-2">
+							<PoliticalParty name="Nasdem" />
+							<PoliticalParty name="PKB" />
+							<PoliticalParty name="PKS" />
+							<PoliticalParty name="Ummat" />
+						</div>
+					</CandidateMainSection>
+					<CandidateMainSection>
+						<CandidatePhoto
+							src="/foto-kandidat/Ganjar_Pranowo.jpg"
+							alt="Potret Resmi Ganjar Pranowo sebagai Gubernur Jawa Tengah Periode 2018-2023"
+							width={799}
+							height={1021}
+						/>
+						<PresidentialCandidateName>
+							Ganjar Pranowo
+						</PresidentialCandidateName>
+						PDIP (19,33% | 128) PPP (4,52% | 19) Hanura (1,54% | 0) Perindo
+						(2,67% | 0)
+						<br />
+						25.57% | 147 Kursi
+						<div className="flex gap-2">
+							<PoliticalParty name="PDIP" />
+							<PoliticalParty name="PPP" />
+							<PoliticalParty name="Hanura" />
+							<PoliticalParty name="Perindo" />
+						</div>
+					</CandidateMainSection>
 				</div>
-			</div>
+				Partai Nasional yang belum mendeklarasikan dukungan Buruh PKN Demokrat
+				(7,77% | 54)
+				<div className="flex gap-2">
+					<PoliticalParty name="Demokrat" />
+					<PoliticalParty name="PKN" />
+					<PoliticalParty name="Buruh" />
+				</div>
+			</main>
+		</div>
+	);
+}
 
-			<div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-				<Image
-					className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-					src="/next.svg"
-					alt="Next.js Logo"
-					width={180}
-					height={37}
-					priority
-				/>
-			</div>
+function CandidateMainSection(props: any) {
+	return (
+		<div className="flex-1 bg-white border-[#CECECE] border rounded p-4">
+			{props.children}
+		</div>
+	);
+}
 
-			<div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-				<a
-					href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Docs{' '}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Find in-depth information about Next.js features and API.
-					</p>
-				</a>
+function PresidentialCandidateName(props: any) {
+	return <h2 className="text-xl">{props.children}</h2>;
+}
 
-				<a
-					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Learn{' '}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Learn about Next.js in an interactive course with&nbsp;quizzes!
-					</p>
-				</a>
+function CandidatePhoto(props: React.ComponentProps<typeof Image>) {
+	return (
+		// eslint-disable-next-line jsx-a11y/alt-text
+		<Image className="w-28 h-36 object-cover" priority {...props} />
+	);
+}
 
-				<a
-					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Templates{' '}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Explore the Next.js 13 playground.
-					</p>
-				</a>
-
-				<a
-					href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Deploy{' '}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Instantly deploy your Next.js site to a shareable URL with Vercel.
-					</p>
-				</a>
-			</div>
-		</main>
+function PoliticalParty(props: any) {
+	return (
+		<div className="inline-flex flex-col content-center">
+			{/* <img
+				src="https://upload.wikimedia.org/wikipedia/id/thumb/c/cd/Logo_PDI-P.svg/360px-Logo_PDI-P.svg.png"
+				alt="pdip"
+				className="h-10 w-auto object-scale-down"
+			/> */}
+			<p className="font-bold">{props.name}</p>
+		</div>
 	);
 }
