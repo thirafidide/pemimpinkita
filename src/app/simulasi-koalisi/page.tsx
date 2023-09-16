00f678133d4bbd012e993bb0078830bf501f1249
Thirafi Dide <jurnaldide@gmail.com>
2023-09-16 10:55:02 +0700

    Proof of concept

--- src/app/simulasi-koalisi/page.tsx
+++ src/app/simulasi-koalisi/page.tsx
@@ -1,3 +1,15 @@
+'use client';
+
+import { PoliticalPartyId } from '@/political-party/PoliticalParty';
+import { PoliticalPartyChip } from '@/political-party/PoliticalPartyChip';
+import {
+	DndContext,
+	DragEndEvent,
+	useDraggable,
+	useDroppable,
+} from '@dnd-kit/core';
+import { useState } from 'react';
+
 export default function CoalitionSimulation() {
 	return (
 		<main className="flex flex-col gap-12 container p-0 mx-auto">
@@ -18,6 +30,92 @@ export default function CoalitionSimulation() {
 					</p>
 				</div>
 			</div>
+
+			<DnDTestApp />
 		</main>
+	);
+}
+
+function DnDTestApp() {
+	const [nonCoalition, setNonCoalition] = useState<PoliticalPartyId[]>([
+		'BURUH',
+		'DEMOKRAT',
+		'PSI',
+		'PKN',
+	]);
+	const [testCoalition, setTestCoalition] = useState<PoliticalPartyId[]>([
+		'PDIP',
+	]);
+
+	function handleDragEnd(event: DragEndEvent) {
+		const { active, over } = event;
+		const coalition =
+			over?.id === 'test-coalition' ? testCoalition : nonCoalition;
+
+		// @ts-ignore
+		if (!coalition.includes(active.id)) {
+			if (over?.id === 'test-coalition') {
+				// @ts-ignore
+				setTestCoalition([...coalition, active.id]);
+				setNonCoalition(nonCoalition.filter((id) => id !== active.id));
+			} else {
+				// @ts-ignore
+				setNonCoalition([...coalition, active.id]);
+				setTestCoalition(testCoalition.filter((id) => id !== active.id));
+			}
+		}
+	}
+
+	return (
+		<DndContext onDragEnd={handleDragEnd}>
+			<Droppable id="non-coalition">
+				<div className="flex-wrap">
+					{nonCoalition.map((party) => (
+						<Draggable key={party} id={party}>
+							<PoliticalPartyChip id={party} />
+						</Draggable>
+					))}
+				</div>
+			</Droppable>
+
+			<Droppable id="test-coalition">
+				<div className="flex-wrap">
+					{testCoalition.map((party) => (
+						<Draggable key={party} id={party}>
+							<PoliticalPartyChip id={party} />
+						</Draggable>
+					))}
+				</div>
+			</Droppable>
+		</DndContext>
+	);
+}
+
+function Droppable(props: any) {
+	const { isOver, setNodeRef } = useDroppable({
+		id: props.id,
+	});
+
+	return (
+		<div ref={setNodeRef} className={isOver ? 'bg-slate-400' : 'bg-slate-600'}>
+			{props.children}
+		</div>
+	);
+}
+
+function Draggable(props: any) {
+	const { attributes, listeners, setNodeRef, transform } = useDraggable({
+		id: props.id,
+	});
+	const style = transform
+		? {
+				transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+		  }
+		: undefined;
+
+	return (
+		<button ref={setNodeRef} style={style} {...listeners} {...attributes}>
+			{props.children}
+		</button>
 	);
 }
