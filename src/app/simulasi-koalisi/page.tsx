bcf653f4547a935d76f65be9c80d0c8de0d585e9
Thirafi Dide <jurnaldide@gmail.com>
2023-09-18 12:01:47 +0700

    Ubah deskripsi halaman

--- src/app/simulasi-koalisi/page.tsx
+++ src/app/simulasi-koalisi/page.tsx
@@ -92,10 +92,10 @@ export default function CoalitionSimulation() {
 						Saat ini sebagian bakal capres masih belum menentukan pasangan
 						cawapresnya, dan partai-partai politik masih saling tarik-menarik
 						untuk membentuk koalisi untuk mendukung pasangan capres-cawapres
-						masing-masing. Bagaimana kalau Prabowo berhasil menarik Demokrat ke
-						koalisinya? Apa jadinya jika Golkar pindah haluan ke koalisi Anies
-						atau Ganjar? Simulasikan hal tersebut di sini dan lihat potensial
-						perubahan suara dan kekuatan koalisi secara keseluruhan.
+						masing-masing. Bagaimana kalau Ganjar yang berhasil menarik Demokrat
+						ke koalisinya? Apa jadinya jika Golkar pindah haluan ke koalisi
+						Anies atau Ganjar? Simulasikan hal tersebut di sini dan lihat
+						potensial perubahan suara dan kekuatan koalisi secara keseluruhan.
 					</p>
 				</div>
 			</div>
