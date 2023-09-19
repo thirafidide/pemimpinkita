899ea201041c456d1f316d14b3a1ede061b0ac1b
Thirafi Dide <jurnaldide@gmail.com>
2023-09-19 21:38:34 +0700

    Perubahan UI untuk penekanan bahwa hasil hanyalah simulasi

--- src/coalition-simulation/CoalitionSimulationCard.tsx
+++ src/coalition-simulation/CoalitionSimulationCard.tsx
@@ -52,63 +52,88 @@ export function CoalitionSimulationCard(props: CoalitionSimulationCardProps) {
 		totalPreviousDPRSeats += previousDPRSeats || 0;
 	}
 
+	const warningStripeStyle = {
+		borderImage: `repeating-linear-gradient(
+				45deg,
+				#222,
+				#222 4px,
+				#fff600 4px,
+				#fff600 8px
+			) 4 / 1 / 0 stretch`,
+	};
+
 	return (
 		<article
 			id={props.coalitionId}
 			ref={setNodeRef}
 			className={cn(
-				'flex flex-col gap-6 flex-1 rounded-lg border bg-card text-card-foreground shadow-sm p-4',
+				'flex-1 rounded-lg border bg-card text-card-foreground shadow-sm overflow-hidden',
 				{ 'opacity-50': active && !isOver },
 			)}
 		>
-			<div className="flex gap-2">
-				<CandidatePhoto {...props.presidentialCandidate.photo} />
-				{props.vicePresidentialCandidate && (
-					<CandidatePhoto {...props.vicePresidentialCandidate.photo} />
-				)}
-			</div>
-
-			<div>
-				<PresidentialCandidateName>
-					{props.presidentialCandidate.name}
-				</PresidentialCandidateName>
-
-				<VicePresidentialCandidateName>
-					{props.vicePresidentialCandidate?.name}
-				</VicePresidentialCandidateName>
-			</div>
-
-			<p className="flex text-xs flex-col gap-1">
-				<span className="text-3xl font-extralight">
-					{percentageFormatter.format(totalPreviousPollStanding / 100)}
-				</span>
-				<span className="flex items-center gap-1">
-					Perolehan suara partai pendukung pada pemilu 2019
-				</span>
-			</p>
-
-			<section className="flex flex-col gap-3">
-				<div>
-					<p className="scroll-m-20 font-semibold tracking-tight">
-						Simulasi Koalisi
-					</p>
-					<p className="text-sm">
-						<span className="font-bold text-2xl">{totalPreviousDPRSeats}</span>{' '}
-						/ 575 Kursi DPR RI (2019 - 2024)
-					</p>
+			<div
+				className="flex flex-col gap-6 p-4 pb-0 h-full border-solid border-y-4"
+				style={warningStripeStyle}
+			>
+				<div className="flex gap-2">
+					<CandidatePhoto {...props.presidentialCandidate.photo} />
+					{props.vicePresidentialCandidate && (
+						<CandidatePhoto {...props.vicePresidentialCandidate.photo} />
+					)}
 				</div>
 
-				<p className="text-sm">
-					Tekan dan seret partai di bawah ini dan taruh di koalisi yang anda
-					inginkan
+				<div>
+					<PresidentialCandidateName>
+						{props.presidentialCandidate.name}
+					</PresidentialCandidateName>
+
+					<VicePresidentialCandidateName>
+						{props.vicePresidentialCandidate?.name}
+					</VicePresidentialCandidateName>
+				</div>
+
+				<p className="flex text-xs flex-col gap-1">
+					<span className="text-3xl font-extralight">
+						{percentageFormatter.format(totalPreviousPollStanding / 100)}
+					</span>
+					<span className="flex items-center gap-1">
+						Perolehan suara partai pendukung pada pemilu 2019
+					</span>
 				</p>
 
-				<div className="flex gap-2 flex-wrap">
-					{partiesData.map(({ partyId }) => (
-						<DraggablePoliticalPartyChip key={partyId} party={partyId} />
-					))}
+				<section className="flex flex-col gap-3">
+					<div>
+						<p className="scroll-m-20 font-semibold tracking-tight">
+							Koalisi Simulasi
+						</p>
+						<p className="text-sm">
+							<span className="font-bold text-2xl">
+								{totalPreviousDPRSeats}
+							</span>{' '}
+							/ 575 Kursi DPR RI (2019 - 2024)
+						</p>
+					</div>
+
+					<p className="text-sm">
+						Tekan dan seret partai di bawah ini dan taruh di koalisi yang anda
+						inginkan
+					</p>
+
+					<div className="flex gap-2 flex-wrap">
+						{partiesData.map(({ partyId }) => (
+							<DraggablePoliticalPartyChip key={partyId} party={partyId} />
+						))}
+					</div>
+				</section>
+
+				<div className="flex-1 -mt-6" />
+
+				<div className="flex bg-foreground text-background px-4 py-2 -mx-4 text-xs">
+					<p className="font-semibold">Simulasi Koalisi</p>
+					<div className="flex-1" />
+					<p>PemimpinKita.org</p>
 				</div>
-			</section>
+			</div>
 		</article>
 	);
 }
