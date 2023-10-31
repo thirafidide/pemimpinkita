6324119c99787974c4c92231bca412231eebe93b
Thirafi Dide <jurnaldide@gmail.com>
2023-10-31 19:31:10 +0000

    Fix hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="id">
+		<html lang="id" suppressHydrationWarning>
 			<body
 				className={cn(
 					inter.className,
